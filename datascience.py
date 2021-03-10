ce494b44af09ff731185c3afb4923a33e61f13e3
Khoironi Kurnia Syah <55708473+zekhoi@users.noreply.github.com>
2021-03-10 15:41:32 +0800

    Create datascience.py

---
+++ datascience.py
@@ -0,0 +1,29 @@
+# Progress Bar
+from tqdm import tqqdm
+
+#
+import pandas as pd
+import seaborn as sns
+import numpy as np
+
+# Opening Data
+data = pd.read_csv('target.csv')
+
+#Showing top 5 and 5 bottom
+data.head()
+data.tail()
+
+# Showing Columns Title
+data.columns
+
+#Showing n x m 
+data.shape
+
+#Showing mean max min
+data.describe()
+
+#Showing Null Data
+data.isnull().sum()
+
+#Data Visualization
+sns.relplot(x='kolom', y='kolom, data=data)
